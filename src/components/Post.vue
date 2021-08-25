3b364941fa389045962f6d64d1e97cc449953363
yasyars <yasya.rusyda@gmail.com>
2021-08-25 11:14:17 +0700

    Add post in home

---
+++ src/components/Post.vue
@@ -0,0 +1,74 @@
+<template>
+   <table class="table w-75" >
+      <thead class="thead-dark sm">
+        <th>User</th>
+        <th>Post</th>
+        <th>Time</th>
+        <th>Action</th>
+      </thead>
+      <tbody>
+        <tr v-for= "post in posts" :key="post.id">
+          <td>{{post.user.username}}</td>
+          <td>{{post.content}}</td>
+          <td>{{post.timestamp}}</td>
+          <td><a href=#>see comments</a>&nbsp;&nbsp;&nbsp; <a href=#>add comments</a></td>
+        </tr>
+        <!-- <tr>
+          <td>
+            <form>
+            <a class="mr-2" href='/items/<%= item.id %>'> Show</a> 
+            <a class="mr-2" href='/items/<%= item.id %>/edit'> Edit</a>
+             <input name="_method" type="hidden" value="delete">
+            </form>
+          </td>
+        </tr> -->
+        <!-- <tr>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td><a href=#>see comments</a>&nbsp;&nbsp;&nbsp; <a href=#>add comments</a></td>
+        </tr> -->
+      </tbody>
+    </table>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  name: 'HelloWorld',
+  data(){
+    return{
+      posts: []
+    }
+  },
+  created(){
+    console.log("WeLCOM")
+    const url = `http://34.97.129.31:4567/post`
+    axios.get(url)
+    .then(res => {
+      let result = res.data
+      this.posts = result.data
+    })
+
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,32 +2,7 @@
   <div class="home">
     <h1 class="d-flex justify-content-center">Timeline</h1>
     <div class="d-flex justify-content-center">
-    <table class="table w-75" >
-      <thead class="thead-dark sm">
-        <th>User</th>
-        <th>Post</th>
-        <th>Time</th>
-        <th>Action</th>
-      </thead>
-      <tbody>
-        <!-- <tr>
-          <td>
-            <form>
-            <a class="mr-2" href='/items/<%= item.id %>'> Show</a> 
-            <a class="mr-2" href='/items/<%= item.id %>/edit'> Edit</a>
-             <input name="_method" type="hidden" value="delete">
-            </form>
-          </td>
-        </tr> -->
-        <tr>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td><a href=#>see comments</a>&nbsp;&nbsp;&nbsp; <a href=#>add comments</a></td>
-        </tr>
-      </tbody>
-    </table>
-
+      <Post></Post>
       <div class="d-flex flex-column ml-2" >
         <a class="ml-2" href = "/items/new">
         <div class="btn btn-primary ">
@@ -52,12 +27,13 @@
 
 <script>
 // @ is an alias to /src
-// import HelloWorld from '@/components/HelloWorld.vue'
+import Post from '@/components/Post.vue';
 import axios from "axios";
 
 export default {
   name: 'Home',
   components: {
+    Post
   },
   data(){
     return{
@@ -68,7 +44,6 @@ export default {
     const url = `http://34.97.129.31:4567/hashtag/trending`
     axios.get(url)
     .then(res => {
-
       let result = res.data
 
       this.hashtags = result.data
